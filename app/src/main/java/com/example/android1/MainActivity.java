89cfeba2e03eb025498f2bbaec772de66ef3cf6b
“nkw97” <nkwijewardena@gmail.com>
2020-06-06 01:32:02 +0530

    add interfaces

--- app/src/main/java/com/example/android1/MainActivity.java
+++ app/src/main/java/com/example/android1/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.android1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnView=findViewById(R.id.btnSubmit);
+        btnView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                opentable();
+            }
+        });
     }
+
+
+    public void opentable(){
+        Intent intent=new Intent(this, tableView.class);
+        startActivity(intent);
+    }
+
 }
